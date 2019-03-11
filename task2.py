58fb3a61ad59b05c1ee3147db2a9b017abb2dcfc
Edona Kallaba <edona.kallaba@udg.edu.me>
2019-03-11 21:21:24 +0100

    Task 2

--- task2.py
+++ task2.py
@@ -23,13 +23,29 @@
 
 
 def valid_mobile_number(phone_number):
+    if len(phone_number) == 9 or len(phone_number) == 10:
+        return True
+    else:
+        return False
+    if phone_number.startswith("06"):
+        return True
+    else:
+        return False
+    if phone_number[2] in range["3","6","7","8","9"]:
+        return True
+    else:
+        return False
+    if phone_number.isdigit():
+        return True
+    else:
+        return False
 
 
 
 
 def main():
 
-    number_to_test = "0699919991"
+    number_to_test = "72"
     if valid_mobile_number(number_to_test):
         print("Phone number is valid in Montenegro!")
     else:
